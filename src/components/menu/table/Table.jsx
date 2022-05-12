e2046e80ca7d6435f206400b85337ce4d97a12a2
Victor164 <vitya16495@gmail.com>
2022-05-12 14:01:32 +0300

    master

--- src/components/menu/table/Table.jsx
+++ src/components/menu/table/Table.jsx
@@ -3,11 +3,11 @@ import Weather from '../../weather/Weather';
 import './Table.css';
 
 function Table () {
-    return (<div className='grid-container'>
+    return (<div className='new'>
               
-            <div className='item1'>
-            </div>
-            <div className="item2" >
+               {/* <div className='item1'>
+            </div> */}
+               <div className="item2" >
             <a target="_blank" href="https://nochi.com/weather/minsk-17469">
                 <img src="https://w.bookcdn.com/weather/picture/4_17469_1_20_137AE9_160_ffffff_333333_08488D_1_ffffff_333333_0_6.png?scode=2&domid=589&anc_id=38816"  alt="booked.net"/>
                 </a>
@@ -21,4 +21,4 @@ function Table () {
     )
     }
 
-    export default Table;
+    export default Table;   
