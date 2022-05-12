9d18b31ab19da79d274c1ee3e6d74648982e166c
Victor164 <vitya16495@gmail.com>
2022-05-12 12:32:38 +0300

    master

--- src/components/menu/material/Material.jsx
+++ src/components/menu/material/Material.jsx
@@ -4,20 +4,26 @@ import './Material.css';
 function Material () {
 return ( <div className="content1">
 <div></div>
+<div className="item5">
 <button className="btn3"><a href="/level">
     <img src="/images/1111.png" alt="level" ></img>
     <br></br>Уровни воды
     </a></button>
+    </div>
 <div></div>
+<div className="item6">
 <button className="btn3"><a href="/gabarit">
      <img src="/images/12.png" alt="gabarit"></img>
     <br></br>Габариты ВВП
     </a></button>
+    </div>
 <div></div>
+<div className="item7">
 <button className="btn3"><a href="/limit">
 <img src="/images/13.png" alt="limit"></img>
 <br></br>Лимитирующие перекаты
     </a></button>
+</div>
 
 </div>
 )
