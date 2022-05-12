6300b66aea610974340a3b3497b3c8e6191fee53
Victor164 <vitya16495@gmail.com>
2022-05-12 14:59:43 +0300

    master

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -5,11 +5,25 @@ function Menu () {
     
 return ( <div>
 <div className="header">
-    <a href="/table">Cводная информация</a>
-    <a href="/sib">Бюллетени</a>
-    <a href="/material">Материалы</a>
-    <a href="/vvp">Схема ВВП</a>
-    <a href="/login">Вход</a>
+     <a href="/table">Cводная информация</a>
+     <a href="/sib">Бюллетени</a>
+     <a href="/material">Материалы</a>
+     <a href="/vvp">Схема ВВП</a>
+     <a href="/login">Вход</a>
+</div>
+<div className="h">
+<details>
+    <summary>   
+        <span data-css-icon="down" className="m">Меню</span>
+    </summary>
+    <div id="open" className="dropdown-contentt">
+     <a href="/table">Cводная информация</a>
+     <a href="/sib">Бюллетени</a>
+     <a href="/material">Материалы</a>
+     <a href="/vvp">Схема ВВП</a>
+     <a href="/login">Вход</a>
+     </div>
+     </details>
 </div>
 </div>
 
