604a995816aeb093999305ff26de30c28b78ffa1
Victor164 <vitya16495@gmail.com>
2022-06-01 16:10:23 +0300

    master

--- src/components/menu/vvp/VVP.jsx
+++ src/components/menu/vvp/VVP.jsx
@@ -3,19 +3,21 @@ import './VVP.css';
 import { YMaps, Map, Placemark } from "react-yandex-maps";
 
 
+
+
 function VVP () {
 return (<div>
     <div></div>
     <div></div>
-    {/* <YMaps>
+
+<div className="grid-container">
+<div className="item1">
+<YMaps>
     <div>
       <Map defaultState={{ center: [53.902207, 27.561676], zoom: 5 }} />
     </div>
-  </YMaps> */}
-<div className="grid-container">
-<div className="item1">
-
-<iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A484bab4bbd70e81e0c2dc5fa4ab8d9488549414838b0090bf5842b07c61aeb96&amp;source=constructor" width="1700" height="850" frameborder="0"></iframe>  
+  </YMaps>
+{/* <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A484bab4bbd70e81e0c2dc5fa4ab8d9488549414838b0090bf5842b07c61aeb96&amp;source=constructor" width="1700" height="850" frameborder="0"></iframe>   */}
 </div>
 
 </div>
@@ -25,3 +27,4 @@ return (<div>
 }
 
 export default VVP;
+
